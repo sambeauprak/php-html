08d130c0f8b5699a90c082ee05073abb22c8f347
Sambeau <sambeauprak@gmail.com>
2025-08-07 11:59:21 +0200

    logout

--- session.php
+++ session.php
@@ -1,55 +1,9 @@
 <?php
+// À partir de maintenant, tu vas m'enregistrer les infos que je veux
+session_start();
 
+$_SESSION["prenom"] = "Karim";
 
-// print_r($_POST);
-$nbEssais = 0;
-$connexionReussie = false;
-function login($email, $password)
-{
-    $utilisateurs = [
-        ["sambeauprak@gmail.com", "zsA7j#jK"],
-        ["sambeau@gmail.com", "jsA7j#jK"],
-        ["sambeau.prak@gmail.com", "vsA7j#jK"],
-        ["sambeaup@gmail.com", "fsA7j#jK"],
-        ["jeanprak@gmail.com", "hsA7j#jK"],
-    ];
+// print_r($_SESSION);
 
-    for ($j = 0; $j < count($utilisateurs); $j++) {
-        $utilisateurEmail = $utilisateurs[$j][0];
-        $utilisateurMdp = $utilisateurs[$j][1];
-
-        if ($email  == $utilisateurEmail && $password == $utilisateurMdp) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["nbEssais"])) {
-
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    $connexionReussie = login($email, $password);
-
-    $nbEssais = (int) $_POST["nbEssais"];
-    if ($connexionReussie == false) {
-        $nbEssais = $nbEssais + 1;
-    }
-}
-
-?>
-
-<?php if ($connexionReussie == false && $nbEssais < 3) : ?>
-    <form action="post.php" method="post">
-        <input type="hidden" name="nbEssais" value="<?php echo $nbEssais; ?>" />
-        <input type="email" name="email" placeholder="Saisir votre email" />
-        <input type="password" name="password" placeholder="Saisir votre mot de passe" />
-        <input type="submit" value="Se connecter" />
-    </form>
-<?php elseif ($nbEssais >= 3): ?>
-    <h2>Vous avez échoué, bande de nazes !</h2>
-<?php else: ?>
-    <h2>Connecté</h2>
-<?php endif; ?>
+echo $_SESSION["prenom"];
---
+++ session_post.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+
+
+if (isset($_SESSION["nbEssais"]) == false) {
+    $_SESSION["nbEssais"] = 0;
+}
+
+$connexionReussie = false;
+function login($email, $password)
+{
+    $utilisateurs = [
+        ["sambeauprak@gmail.com", "zsA7j#jK"],
+        ["sambeau@gmail.com", "jsA7j#jK"],
+        ["sambeau.prak@gmail.com", "vsA7j#jK"],
+        ["sambeaup@gmail.com", "fsA7j#jK"],
+        ["jeanprak@gmail.com", "hsA7j#jK"],
+    ];
+
+    for ($j = 0; $j < count($utilisateurs); $j++) {
+        $utilisateurEmail = $utilisateurs[$j][0];
+        $utilisateurMdp = $utilisateurs[$j][1];
+
+        if ($email  == $utilisateurEmail && $password == $utilisateurMdp) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $connexionReussie = login($email, $password);
+
+    if ($connexionReussie == false) {
+        echo "Connexion échouée";
+        $_SESSION["nbEssais"] = $_SESSION["nbEssais"] + 1;
+    }
+}
+
+?>
+
+<?php if ($connexionReussie == false && $_SESSION["nbEssais"] < 3) : ?>
+<form action="session_post.php" method="post">
+    <input type="email" name="email" placeholder="Saisir votre email" />
+    <input type="password" name="password" placeholder="Saisir votre mot de passe" />
+    <input type="submit" value="Se connecter" />
+</form>
+<?php elseif ($_SESSION["nbEssais"] >= 3): ?>
+<h2>Vous avez échoué, bande de nazes !</h2>
+<?php else: ?>
+<h2>Connecté</h2>
+<a href="logout.php">Déconnexion</a>
+<?php endif; ?>
